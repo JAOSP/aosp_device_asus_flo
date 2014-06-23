f3f724e8ba34a1dfb60ec109a0a76ef11f229fc0
Akihiro Maeda <sola.1980.a@gmail.com>
2014-06-23 22:56:55 +0900

    Updated Android 4.4.4

--- full_flo.mk
+++ full_flo.mk
@@ -21,7 +21,7 @@
 # Inherit from the common Open Source product configuration
 $(call inherit-product, $(SRC_TARGET_DIR)/product/aosp_base.mk)
 
-PRODUCT_BUILD_PROP_OVERRIDES += PRODUCT_NAME=razor BUILD_FINGERPRINT=google/razor/flo:4.4.3/KTU84L/1148727:user/release-keys PRIVATE_BUILD_DESC="razor-user 4.4.3 KTU84L 1148727 release-keys"
+PRODUCT_BUILD_PROP_OVERRIDES += PRODUCT_NAME=razor BUILD_FINGERPRINT=google/razor/flo:4.4.4/KTU84P/1227136:user/release-keys PRIVATE_BUILD_DESC="razor-user 4.4.4 KTU84P 1227136 release-keys"
 
 PRODUCT_NAME := full_flo
 PRODUCT_DEVICE := flo
